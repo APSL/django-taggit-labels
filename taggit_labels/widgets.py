40ed182ca6e9d68e12c9fed65283442357ec77c5
Tomeu Canyelles <bce892@gmail.com>
2016-11-15 21:02:54 +0100

    Reformat widget class * Reformat code of LabelWidget to get more flexibility to exends and overwite it * Set 'slug' as default filter key

--- taggit_labels/widgets.py
+++ taggit_labels/widgets.py
@@ -5,6 +5,7 @@ try:
 except ImportError:
     # Django <1.9
     from django.forms.util import flatatt
+from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils import six
 
@@ -17,6 +18,8 @@ class LabelWidget(forms.TextInput):
     Widget class for rendering an item's tags - and all existing tags - as
     selectable labels.
     """
+    template_name = 'taggit_label_widget.html'
+    lookup_field = 'slug'
     input_type = 'hidden'
     model = Tag
 
@@ -28,29 +31,46 @@ class LabelWidget(forms.TextInput):
     def is_hidden(self):
         return False
 
+    def get_class(self, value, tags):
+        """
+        Mark tag label field as selected according to the values list
+        :param value: evaluating inclusion tag into selected
+        :param tags: all available tags
+        """
+        return 'selected' if value in tags else ''
+
     def tag_list(self, tags):
         """
         Generates a list of tags identifying those previously selected.
 
-        Returns a list of tuples of the form (<tag name>, <CSS class name>).
+        Returns a list of python dict with from data:
+        <slug tag>, <tag name> and <CSS class name>.
 
-        Uses the string names rather than the tags themselves in order to work
-        with tag lists built from forms not fully submitted.
+        Changing lookup_field attribute you can filter by other model Tag fields.
         """
-        return [(tag.name, 'selected' if tag.name in tags else '')
-                for tag in self.model.objects.all()]
+        return [
+            {'value_field': getattr(tag, self.lookup_field, ''), 'label_field': tag.name,
+             'field_class': self.get_class(getattr(tag, self.lookup_field, ''), tags)}
+            for tag in self.model.objects.all()
+        ]
 
     def format_value(self, value):
         if value is not None and not isinstance(value, six.string_types):
             value = edit_string_for_tags([o.tag for o in value.select_related("tag")])
         return value
 
+    def get_extra_context_data(self):
+        """
+        Allow an easy override context before render input field.
+        """
+        return {}
+
     def render(self, name, value, attrs={}):
         # Case in which a new form is dispalyed
         if value is None:
             current_tags = []
             formatted_value = ""
-            selected_tags = self.tag_list([])
+            selected_tags = self.tag_list(current_tags)
 
         # Case in which a form is displayed with submitted but not saved
         # details, e.g. invalid form submission
@@ -63,7 +83,7 @@ class LabelWidget(forms.TextInput):
         else:
             current_tags = [o.tag for o in value.select_related("tag")]
             formatted_value = self.format_value(value)
-            selected_tags = self.tag_list([t.name for t in current_tags])
+            selected_tags = self.tag_list([getattr(t, self.lookup_field, '') for t in current_tags])
 
         input_field = super(LabelWidget, self).render(name, formatted_value, attrs)
 
@@ -71,10 +91,15 @@ class LabelWidget(forms.TextInput):
             attrs.update({'class': 'tags taggit-labels'})
         list_attrs = flatatt(attrs)
 
-        tag_li = "".join([u"<li data-tag-name='{0}' class={1}>{0}</li>".format(
-            tag[0], tag[1]) for tag in selected_tags])
-        tag_ul = u"<ul{0}>{1}</ul>".format(list_attrs, tag_li)
-        return mark_safe(u"{0}{1}".format(tag_ul, input_field))
+        context = {
+            'input_field': input_field,
+            'selected_tags': selected_tags,
+            'list_attrs': list_attrs
+        }
+        context.update(self.get_extra_context_data())
+
+        template_content = render_to_string(self.template_name, context)
+        return mark_safe(template_content)
 
     class Media:
         css = {
--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -13,9 +13,10 @@ class LabelTest(TestCase):
     def setUp(self):
         Tag.objects.create(name="Python", slug="python")
         Tag.objects.create(name="Django", slug="django")
-        Tag.objects.create(name="Advanced Computering", slug="advanced-computering")
+        Tag.objects.create(name="Advanced Computering",
+                           slug="advanced-computering")
         MyCustomTag.objects.create(name="Coffee", slug="coffee")
-        MyCustomTag.objects.create(name="tea", slug="tea")
+        MyCustomTag.objects.create(name="Tea", slug="tea")
         MyCustomTag.objects.create(name="À bientôt", slug="a-bientot")
         self.article = Content.objects.create(title="My test")
         self.article.tags.add("Python")
@@ -30,18 +31,26 @@ class LabelTest(TestCase):
 
     def test_selected_tags(self):
         widget = LabelWidget()
-        return_list = widget.tag_list([t.name for t in self.article.tags.all()])
-        self.assertEqual(["Python"], [tag[0] for tag in return_list if tag[1] == 'selected'])
-        self.assertEqual(["Django", "Advanced Computering"], [tag[0] for tag in return_list if tag[1] == ''])
+        return_list = widget.tag_list([getattr(t, widget.lookup_field, '')
+                                       for t in self.article.tags.all()])
+        self.assertEqual(["python"], [tag['value_field'] for tag in return_list
+                                      if tag['field_class'] == 'selected'])
+        self.assertEqual(["django", "advanced-computering"],
+                         [tag['value_field'] for tag in return_list
+                          if tag['field_class'] == ''])
 
     def test_custom_selected_tags(self):
         widget = LabelWidget(model=MyCustomTag)
-        return_list = widget.tag_list([t.name for t in self.post.tags.all()])
-        self.assertEqual(["Coffee"], [tag[0] for tag in return_list if tag[1] == 'selected'])
-        self.assertEqual(["tea"], [tag[0] for tag in return_list if tag[1] == ''])
+        return_list = widget.tag_list([getattr(t, widget.lookup_field, '')
+                                       for t in self.post.tags.all()])
+        self.assertEqual(["coffee"], [tag['value_field'] for tag in return_list
+                                      if tag['field_class'] == 'selected'])
+        self.assertEqual(["tea"], [tag['value_field'] for tag in return_list
+                                   if tag['field_class'] == ''])
 
     def test_render_new(self):
         """Render method shouldn't error out with missing or string tags"""
         widget = LabelWidget(model=MyCustomTag)
         widget.render("tags", None, attrs={'id': u'id_tags'})
-        widget.render("tags", "'My Tag', 'Another tag'", attrs={'id': u'id_tags'})
+        widget.render("tags", "'My Tag', 'Another tag'",
+                      attrs={'id': u'id_tags'})
